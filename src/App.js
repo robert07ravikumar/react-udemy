fa1e7c4029aac922105610a8aea32ee89c7b76fa
Dell\Mabel <robert07ravikumar@gmail.com>
2019-10-29 00:10:16 +0800

    react props state react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
+const App = props => {
+
+  const [ pesrsonState , setPersonState ] = useState({
+    persons:[
+      {name:'robert', age : 34},
+      {name:'janice',age: 4},
+      {name:'rachel',age: 35}
+    ]
+  });
+
+  const [ otherState , setOtherState ] = useState('some randomvalue');
+
+  const swicthNameHandler = () => {
+    
+    setPersonState({
+      persons:[
+        {name:'Maria', age : 34},
+        {name:'anand',age: 4},
+        {name:'rachel', age : 5}
+      ]
+    });
+    console.log(pesrsonState,otherState);
+  };
+
+  console.log(pesrsonState,otherState);
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Hi I , a react app</h1> 
+    <p>This is a really working</p>
+    <button onClick={swicthNameHandler}>Switch Name</button>
+    <Person name={pesrsonState.persons[0].name} age={pesrsonState.persons[0].age}></Person>
+    <Person name={pesrsonState.persons[1].name} age={pesrsonState.persons[1].age}>My hobbiles is racing</Person>
+    <Person name={pesrsonState.persons[2].name} age={pesrsonState.persons[2].age}></Person>
     </div>
-  );
+  )
 }
 
 export default App;
